2d80843f013de818e602fb3e24b50855f05e236c
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-05 03:08:16 +0300

    feat: implement create card foo

---
+++ js/createCard.js
@@ -0,0 +1,23 @@
+const createCard = (items) => {
+
+    let innerHTML = ''
+
+    items.forEach(item => {
+
+        innerHTML +=
+            `            
+            <article id="${item.name}" 
+                class="hidden" onmouseover = "this.classList.remove('hidden')" 
+                onmouseout = "this.classList.add('hidden')"
+            >
+                <div class="picture__note">
+                    <h3 class="picture__title">${item.name}</h3>
+                    <p>${item.description}</p>
+                </div>
+            </article>`
+    })
+
+    return innerHTML
+}
+
+export default createCard
