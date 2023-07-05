a15102529c5609f4907d8d0624a2efa6d2b95863
RimidalU <stankevichuuladimiru@gmail.com>
2023-07-05 03:09:27 +0300

    feat: add gallery to app

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
+import shuffleGallery from "./shuffleGallery.js";
+import createImage from "./createImage.js";
+import createCard from "./createCard.js";
+
 import { gallery } from "../data/gallery.js";
 
+const gallerySet = document.getElementById("gallery");
+
+const createGallery = () => {
+	shuffleGallery(gallery);
+
+	gallerySet.innerHTML = createCard(gallery);
+
+	for (let item of gallery) {
+		const parent = document.getElementById(item.name);
+		createImage(item, parent);
+	}
+};
+
+window.onload = createGallery();
